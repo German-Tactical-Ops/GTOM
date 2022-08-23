0d3fdcf911817ea8bb17de510b81e5183db439cb
Mateusz <mateuszhwg@gmail.com>
2022-08-23 13:00:51 +0200

    small changes

--- @gtom/mod.cpp
+++ @gtom/mod.cpp
@@ -1,32 +1,32 @@
-        name = "GTOM"; // Full name of the mod
+name = "GTOM"; // Full name of the mod
 
-        author = "LucyferHW";
+author = "LucyferHW";
 
-        picture = "GTOM-a.paa";
+picture = "GTOM-a.paa";
 
-        overviewPicture = "GTOM-b.paa";
+overviewPicture = "GTOM-b.paa";
 
-        actionName = "GitHub";
+//actionName = "GitHub";
 
-        action = "https://github.com/MHWcreations/GTO_Mod";
+//action = "https://github.com/MHWcreations/GTO_Mod";
 
-        description = "This mod is made for the GTO Unit. Everybody can use it, but it will only work as intended with the GTO-modpack. This mod contains many small things for Players, Curators and Mission Makers.";
+description = "This mod is made for the GTO Unit. Everybody can use it, but it will only work as intended with the GTO-modpack. This mod contains many small things for Players, Curators and Mission Makers.";
 
-        logo = "GTOM-a.paa"; // Logo displayed in bottom left of main menu
+logo = "GTOM-a.paa"; // Logo displayed in bottom left of main menu
 
-        logoOver = "GTOM-hover.paa"; // Logo displayed in the mod menu when mod icon is clicked
+//logoOver = "GTOM-hover.paa"; // Logo displayed in the mod menu when mod icon is clicked
 
-        // logoSmall = "gtom\logo\gto_unit_logo.paa"; // Logo displayed next to all units and vehicles in the editor
+// logoSmall = "gtom\logo\gto_unit_logo.paa"; // Logo displayed next to all units and vehicles in the editor
 
-        tooltip = "GTOM";
-        tooltipOwned = "GTOM Owned";
+tooltip = "GTOM";
+tooltipOwned = "GTOM Owned";
 
-        overview = "Version: 1.1.1<br/><br/>This mod is made for the GTO Unit. Everybody can use it, but it will only work as intended with the GTO-modpack. This mod contains many small things for Players, Curators and Mission Makers.";
+overview = "Version: 1.1.1<br/><br/>This mod is made for the GTO Unit. Everybody can use it, but it will only work as intended with the GTO-modpack. This mod contains many small things for Players, Curators and Mission Makers.";
 
-        // fieldManualTopicAndHint[] = {"Curator", "Curator"};
+// fieldManualTopicAndHint[] = {"Curator", "Curator"};
 
-        // dlcColor[] = {0.31, 0.78, 0.78, 1}; // Something about color, just leave it like this
+// dlcColor[] = {0.31, 0.78, 0.78, 1}; // Something about color, just leave it like this
 
-        // hideName = 0; // Hide the name of the mod
+// hideName = 0; // Hide the name of the mod
 
-        // hidePicture = 0; // Hide the logo of the mod
+// hidePicture = 0; // Hide the logo of the mod
