df8c778681841d927377b4d867700a0ef8be7c5d
MHWcreations <mateuszhwg@gmail.com>
2022-11-08 20:11:27 +0100

    added extendedArsenal

---
+++ gtom/extendedArsenal/config.cpp
@@ -0,0 +1,19 @@
+#include "\gtom\basicDefines_A3.hpp"
+#include "\gtom\defines.hpp"
+
+class CfgPatches
+{
+    class GTOM_ExtendedArsenal
+    {
+        name = "GTO Main";
+        author = "LucyferHW";
+        url = "";
+
+        units[] = {};
+
+        requiredAddons[] = {"GTOM_Main"};
+    };
+};
+
+#include "configs\XtdGearModels.hpp"
+#include "configs\XtdGearInfos.hpp"
