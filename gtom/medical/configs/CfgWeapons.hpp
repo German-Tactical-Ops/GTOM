724742ffdcae783e237943bd8efbd242adeb7f85
MHWcreations <mateuszhwg@gmail.com>
2022-11-14 23:14:42 +0100

    made base item class

---
+++ gtom/medical/configs/CfgWeapons.hpp
@@ -0,0 +1,21 @@
+class CfgWeapons {
+
+	class Item_FirstAidKit;
+	class Item_Medikit;
+
+	class GTO_FirstAidKit : Item_FirstAidKit
+	{
+		displayName = "GTO FirstAidKit";
+		author = "LucyferHW";
+		scope = 2;
+		//picture = "\A3\characters_f\Data\UI\icon_V_BandollierB_CA.paa";
+	};
+
+	class GTO_Medikit : Item_Medikit
+	{
+		displayName = "GTO Medikit";
+		author = "LucyferHW";
+		scope = 2;
+		//picture = "\A3\characters_f\Data\UI\icon_V_BandollierB_CA.paa";
+	};
+};
