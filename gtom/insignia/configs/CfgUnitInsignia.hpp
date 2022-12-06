9897edd964c42b3cd9b81608f9efca47af2c23ef
MHWcreations <mateuszhwg@gmail.com>
2022-12-06 16:32:17 +0100

    made basic structure

---
+++ gtom/insignia/configs/CfgUnitInsignia.hpp
@@ -0,0 +1,21 @@
+class CfgUnitInsignia
+{
+
+    class gto_insignia
+    {
+        displayName = "GTO";
+        author = "LucyferHW";
+        material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+        texture = "gtom\logo\gto_unit_logo.paa";
+        textureVehicle = "gtom\logo\gto_unit_logo.paa";
+    };
+
+    class gto_insignia_medic
+    {
+        displayName = "Medic";
+        author = "w0llkopf";
+        material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+        texture = "gtom\wearables\icons\gto_medic_icon.paa";
+        textureVehicle = "gtom\wearables\icons\gto_medic_icon.paa";
+    };
+};
