56bcd81979df521bbcc7147a06e3abf46b0e9fe3
Mateusz <mateuszhwg@gmail.com>
2023-02-18 02:28:54 +0100

    created first files

---
+++ gtom/ranksystem/configs/CfgFunctions.hpp
@@ -0,0 +1,18 @@
+#include "\gtom\basicDefines_A3.hpp"
+
+class cfgFunctions
+{
+	class GTO
+	{
+		class rankSystem
+		{
+			file = "gtom\ranksystem\functions";
+
+			class cbaSettingsRanksystem
+			{
+				postInit = 1;
+				recompile = 1;
+			}; // [] call GTO_fnc_cbaSettingsRanksystem;
+		};
+	};
+};
---
+++ gtom/ranksystem/functions/fn_cbaSettingsRanksystem.sqf
@@ -0,0 +1,11 @@
+scriptName "fn_cbaSettingsRanksystem";
+/*
+	Author: LucyferHW
+	
+	Description:
+	Runs on start.
+	
+	Parameter(s):
+	#0 OBJECT - Description
+*/
+
