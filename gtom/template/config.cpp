b0f593ac8063b8969f907f2cf5e4e04075ca06d2
Mateusz <mateuszhwg@gmail.com>
2023-02-20 22:40:15 +0100

    added template structure

---
+++ gtom/template/config.cpp
@@ -0,0 +1,16 @@
+#include "\gtom\basicDefines_A3.hpp"
+
+
+class CfgPatches
+{
+    class GTOM_Template
+    {
+        name = "GTO Template";
+        author = "LucyferHW";
+        url = "";
+
+        units[] = {};
+
+        requiredAddons[] = {"GTOM_Main"};
+    };
+};
