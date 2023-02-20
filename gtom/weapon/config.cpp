8272eaae8d5a6dd48e5a2ad8dcbb1adcec7dd8b9
Mateusz <mateuszhwg@gmail.com>
2023-02-20 22:50:30 +0100

    made changes to weapon sway

---
+++ gtom/weapon/config.cpp
@@ -0,0 +1,18 @@
+#include "\gtom\basicDefines_A3.hpp"
+
+
+class CfgPatches
+{
+    class GTOM_Weapon
+    {
+        name = "GTO Weapon";
+        author = "LucyferHW";
+        url = "";
+
+        units[] = {};
+
+        requiredAddons[] = {"A3_Data_F","GTOM_Main"};
+    };
+};
+
+#include "configs\CfgImprecision.hpp"
