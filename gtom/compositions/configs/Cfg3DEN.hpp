63b2e801d9753c9310b03055e403739aa622a26a
Lucyfer Angel <50595135+LucyferHW@users.noreply.github.com>
2024-11-04 18:55:48 +0100

    added new composition

--- gtom/compositions/configs/Cfg3DEN.hpp
+++ gtom/compositions/configs/Cfg3DEN.hpp
@@ -13,6 +13,17 @@ class Cfg3DEN
             useSideColorOnIcon = 1;                            // 1 == icon is always colored in faction color
         };
 
+        class GTO_Army_v2 // one class per composition
+        {
+            path = "gtom\compositions\GTO_Army_v2";             // pbo path to a folder containing header.sqe/composition.sqe files
+            side = 1;                                           // 0 opfor, 1 blufor, 2 indfor, 3 civ, 8 Empty/Props
+            editorCategory = "Categorie_GTO";                   // link to CfgEditorCategories
+            editorSubcategory = "Categorie_GTO_Infantry";       // link to CfgEditorSubcategories
+            displayName = "GTO Army v2";
+            icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";  // left side icon in groups list
+            useSideColorOnIcon = 1;                             // 1 == icon is always colored in faction color
+        };
+
         class GTO_MainSystem // one class per composition
         {
             path = "gtom\compositions\MainSystem";  // pbo path to a folder containing header.sqe/composition.sqe files
