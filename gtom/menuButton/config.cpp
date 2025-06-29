86ca8be4f4c950ca83ad22b2feda23d5837cbe52
Lucyfer Angel <50595135+LucyferHW@users.noreply.github.com>
2025-06-29 19:21:15 +0200

    added new button to menu

--- gtom/menuButton/config.cpp
+++ gtom/menuButton/config.cpp
@@ -32,23 +32,23 @@ class CfgMainMenuSpotlight
         picture = "gtom\images\logo\MainMenu_2.paa";
         action = "connectToServer ['46.4.37.180', 2312, 'asdf0987']";
         actionText = "Join Britain Server";
-        condition = "true";
+        condition = "false";
     };
 
-    class JoinServerGTOHalo
+    class JoinServerGTOFunOp
     {
-        text = "HALO";
-        picture = "gtom\images\logo\MainMenu_1.paa";
+        text = "FunOp";
+        picture = "gtom\images\logo\Arma3-factionlogo-wsion.paa";
         action = "connectToServer ['46.4.37.180', 2322, 'asdf0987']";
-        actionText = "Join HALO Server";
-        condition = "false";
+        actionText = "Join FunOp Server";
+        condition = "true";
     };
 
     class JoinServerGTORanger
     {
         text = "Ranger";
         picture = "gtom\images\logo\MainMenu_3.paa";
-        action = "connectToServer ['46.4.37.180', 2332, 'D4rkSp1d3r']";
+        action = "connectToServer ['46.4.37.180', 2332, 'RangersLeadTheWay']";
         actionText = "Join Ranger Server";
         condition = "true";
     };
